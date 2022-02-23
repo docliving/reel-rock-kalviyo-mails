30c11bf6fe5c4e12d89148a1b722b014fe03331c
JC Living <doclivingmx@gmail.com>
2022-02-23 08:41:06 -0600

    x

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 
-
+en el head con codigo 302 /moved temoprarary"
 entrada = "http://vps1.reelrocktour.com/endpoint_node/cupon";
 
 Location = "https://reelrocktour.com/cart/42400410599656:1?discount="+cupon;
